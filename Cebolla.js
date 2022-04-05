fed95a3b2df5428ead4dc2fbe76c9c79fd09feb3
faksu <dk>
2022-04-05 11:44:24 -0300

    principio

---
+++ Cebolla.js
@@ -0,0 +1,3 @@
+document.write ("<h1> El brindis con el cebolla </h1>")
+
+document.write ("<img src='https://p4.wallpaperbetter.com/wallpaper/726/651/712/dark-souls-dark-souls-3-dark-souls-ii-dark-souls-iii-solaire-of-astora-hd-wallpaper-preview.jpg' alt='brindis con el cebolla'>")
